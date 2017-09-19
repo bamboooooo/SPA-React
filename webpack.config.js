8d620b4fd9f4d06a6c9d55162e7b5f7dbd46c65d
maxuezhu <maxuezhu@188w.com>
2017-09-19 16:29:19 +0800

    升级webpack版本

--- webpack.config.js
+++ webpack.config.js
@@ -1,76 +1,145 @@
 const webpack = require('webpack')
 const HtmlWebpackPlugin=require('html-webpack-plugin');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
+var path = require('path');
 module.exports = {
     entry: {
-        main: './src/index.js'
-        vendors: ['react','antd']
+        main: path.join(__dirname, 'src/index'),
+        vendor: ['react', 'react-dom', 'react-router', 'antd']
     },
     output: {
-        path: './dist',
-        filename: '/js/build.js'
+        path: path.resolve(__dirname, './dist'),
+        filename:  'js/[name].[hash].js',
+        chunkFilename: 'js/[name].chunk.js'
     },
     devServer: {
         hot: true,
         port: 8088,
         inline: true,
-        open: true
+        open: true,
+        progress: true
     },
     module: {
-        loaders: [
+           rules: [
             {
-                // edit this for additional asset file types
-                test: /\.(png|jpg|gif)$/, 
-                loader: 'url-loader?limit=819200'
+                test: /\.js?$/,
+                loader: 'babel-loader',
+                exclude:path.resolve(__dirname, "node_modules"),
+                include: path.resolve(__dirname, "src"),
+                options:{
+                    cacheDirectory: true
+                }
             },
             {
-                test: /\.js$/,
-                // excluding some local linked packages.
-                // for normal use cases only node_modules is needed.
-                exclude: /node_modules|vue\/dist|vue-router\/|vue-loader\/|vue-hot-reload-api\//,
-                loader: 'babel'
+                test: /\.(jpe?g|png|gif|svg|ico)/i,
+                use: [
+                    {
+                        loader: 'url-loader',
+                        options: {
+                            limit: 10000,
+                            name: 'asset/media/[name].[hash:8].[ext]',
+                        }
+                    }
+                ]
             },
-            {   test: /\.css$/, 
-            	 loader:  ExtractTextPlugin.extract("style-loader","css-loader?sourceMap")
+            {
+                test: /\.(ttf|eot|svg|woff|woff2|pdf|swf|xap)/,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {}
+                    }
+                ]
             },
-         
-            { 
-                test: /\.(woff|svg|eot|ttf)\??.*$/,
-                loader: 'url-loader?limit=50000&name=[path][name].[ext]'
-            }
+             {
+		        test: /\.css$/,
+		        use: ExtractTextPlugin.extract({
+		          fallback: "style-loader",
+		          use:["css-loader"]
+		        })
+		     }
         ]
     },
-    // example: if you wish to apply custom babel options
-    // instead of using vue-loader's default:
-    babel: {
-        presets: ['es2015', 'stage-0', 'react'],
-        plugins: ['transform-runtime', ["antd",  { "style": "css" }]]
-    },
     plugins: [
-    	new ExtractTextPlugin("styles.css"),
-        new webpack.optimize.CommonsChunkPlugin('vendors', 'vendors.js', Infinity) // 这是第三方库打包生成的文件
+        new HtmlWebpackPlugin()
     ]
 }
-
 if (process.env.NODE_ENV !== 'production') {
+    module.exports = {
+        devtool: 'source-map',
+        entry: {
+            main: path.join(__dirname, 'src/index'),
+            vendor: ['react', 'react-dom', 'react-router', 'antd']
+        },
+        output: {
+            path: path.resolve(__dirname, './dist'),
+            filename:  'js/[name].[hash].js',
+            chunkFilename: 'js/[name].chunk.js'
+        },
+        devServer: {
+            hot: true,
+            port: 8088,
+            inline: true,
+            open: true,
+            progress: true
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.js?$/,
+                    loader: 'babel-loader',
+                    exclude:path.resolve(__dirname, "node_modules"),
+                    include: path.resolve(__dirname, "src"),
+                    options:{
+                        cacheDirectory: true
+                    }
+
+                },
+                {
+                    test: /\.(jpe?g|png|gif|svg|ico)/i,
+                    use: [
+                        {
+                            loader: 'url-loader',
+                            options: {
+                                limit: 10000,
+                                name: 'asset/media/[name].[hash:8].[ext]',
+                            }
+                        }
+                    ]
+                },
+                {
+                    test: /\.(ttf|eot|svg|woff|woff2|pdf|swf|xap)/,
+                    use: [
+                        {
+                            loader: 'file-loader',
+                            options: {}
+                        }
+                    ]
+                },
+                {
+                    test: /\.css$/,
+                    use: ['style-loader', 'css-loader']
+                }
+            ]
+        },
+        plugins: [
+            new HtmlWebpackPlugin()
+        ]
+    }
+}else {
+
     module.exports.plugins = [
-        new webpack.DefinePlugin({
-            'process.env': {
-                NODE_ENV: JSON.stringify('production')
-            }
-        }),
+        new HtmlWebpackPlugin(),
         new webpack.optimize.UglifyJsPlugin({
             compress: {
                 warnings: false
             }
         }),
-        new webpack.optimize.OccurenceOrderPlugin(),
-        new HtmlWebpackPlugin({
-            template: "./src/index.html",
-            inject: true,
-            hash:true
-        })
+        new webpack.optimize.CommonsChunkPlugin({
+            name: "vendor",
+            minChunks: Infinity
+        }),
+        new ExtractTextPlugin("css/[name].[hash].css")
     ]
-} else {
-    module.exports.devtool = '#source-map'
+
 }
